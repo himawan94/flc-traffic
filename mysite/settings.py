8a6b38d389622b4d0e393688f9743d990f4270eb
Himawan <tugasakhir309@gmail.com>
2018-01-21 21:04:32 +0800

    My Django Fuzzy app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tzrx75)s9aeq^k!^bms0$*)jdk4pc!a8r-^_(z+b8v#39mx5$)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
