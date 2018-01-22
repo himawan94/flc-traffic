0aa3d7d3305630b7a10adb21cb4a309f972f039b
Himawan <tugasakhir309@gmail.com>
2018-01-22 12:51:58 +0800

    My Django Fuzzy app, first commit

--- webapp/models.py
+++ webapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class vehicle(models.Model):
     num_east=models.IntegerField()
-    north_east = models.IntegerField()
-    south_east = models.IntegerField()
+    num_north=models.IntegerField()
+    num_south=models.IntegerField()
